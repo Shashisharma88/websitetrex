5e286b4d4a4442ea6e8636d0c81d89b4efc729c4
Shashi <shashisharma882002@gmail.com>
2024-02-12 11:00:11 +0530

    updated body

--- src/components/Body.js
+++ src/components/Body.js
@@ -35,6 +35,23 @@ const Body =()=>{
             console.log(error)
         })
     }
+
+    const submitHandler = (event)=>{
+        event.preventDefault();
+         const email_address= event.target.email.value;
+         axios.post("https://api.trex20.xyz/api/v1/subscribe",{
+            email_address
+         })
+         .then((response)=>{
+            console.log(response);
+            event.target.reset();
+         })
+         .catch((error) =>{
+            console.log(error);
+         });
+    };
+
+
     return(<>
     <section id="landing">
         <div className="home" >
@@ -74,14 +91,14 @@ const Body =()=>{
             <div className="contact-right">
             
             <div style={{ fontFamily: 'Heaven' }} className="subscribe">SUBSCRIBE :</div>
-            <form className="collect" onSubmit={handleSubmit}>
+            <form className="collect" onSubmit={submitHandler} >
            
-            <input style={{fontFamily:'Pixelify Sans'}} placeholder="Enter your email" type="email" id="email_address"  name="email_address" value={inputData.email_address} onChange={handleData} onClick={handleSubmit} /> <t/>
+            <input style={{fontFamily:'Pixelify Sans'}} placeholder="Enter your email" type="email" id="email"  name="email" /> <t/>
             <input style={{fontFamily:'Heaven', cursor:"pointer"}}  type="submit" value="Submit"  />
             </form>
-            <a href="https://t.me/trex20OfficialChat">
+            <a href="https://twitter.com/trex20_official " target="_blank">
             <img  className="twitter" alt="twitter_image" src={twitter} /></a>
-            <a href="https://twitter.com/trex20_official">
+            <a href="   https://t.me/trex20OfficialChat" target="_blank">
             <img  className="telegram" alt="telegram_image" src={telegram} /></a>
             </div>
         </div>
@@ -91,3 +108,84 @@ const Body =()=>{
     );
 }
 export default Body;
+
+
+
+
+
+
+
+// const Body =()=>{
+//     const data = {email_address:""};
+//     const [inputData,setInputData]= useState(data)
+    
+//     const handleData = (e)=>{
+//         setInputData({...inputData,[e.target.name]:e.target.value})
+//     }
+//     const handleSubmit = (e)=>{
+//         e.preventDefault();
+//         axios.post("https://api.trex20.xyz/api/v1/subscribe",inputData)
+//         .then((response)=>{
+//             console.log(response)
+//             e.target.reset();
+//         })
+//         .catch((error) =>{
+//             console.log(error)
+//         })
+//     }
+//     return(<>
+//     <section id="landing">
+//         <div className="home" >
+           
+//         </div>
+//     </section>  
+//     <section  id="trex">  
+//         <div className="trex-body">
+            
+//         </div>
+//     </section>
+//     <section id="inscription">
+//         <div className="inscription-body-1">
+        
+//         </div>
+//         <div className="inscription-body-2">
+        
+//         </div>
+//     </section>
+//     <section id="lotto">
+//         <div className="lotto-body">
+        
+//         </div>
+//     </section>
+//     <section id="roadmap">
+//         <div className="roadmap-body">
+            
+//         </div>
+//     </section>
+//     <section id="contact">
+//         <div style={{ fontFamily: 'Heaven' }} className="contact" >CONTACT</div>
+//         <div className="contact-body">
+            
+//             <div className="contact-left">
+//             <img  className="inbox" alt="Inbox_image" src={inbox} />
+//             </div>
+//             <div className="contact-right">
+            
+//             <div style={{ fontFamily: 'Heaven' }} className="subscribe">SUBSCRIBE :</div>
+//             <form className="collect" onSubmit={handleSubmit}>
+           
+//             <input style={{fontFamily:'Pixelify Sans'}} placeholder="Enter your email" type="email" id="email_address"  name="email_address" value={inputData.email_address} onChange={handleData} onClick={handleSubmit} /> <t/>
+//             <input style={{fontFamily:'Heaven', cursor:"pointer"}}  type="submit" value="Submit"  />
+//             </form>
+//             <a href="https://twitter.com/trex20_official " target="_blank">
+//             <img  className="twitter" alt="twitter_image" src={twitter} /></a>
+//             <a href="   https://t.me/trex20OfficialChat" target="_blank">
+//             <img  className="telegram" alt="telegram_image" src={telegram} /></a>
+//             </div>
+//         </div>
+//     </section>
+    
+//         </>
+//     );
+// }
+// export default Body;
